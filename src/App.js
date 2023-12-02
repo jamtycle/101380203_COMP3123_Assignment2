55089544e8683eaeb80121668663db2290e21939
Bruno Ramirez <bramirez206@hotmail.com>
2023-12-01 21:33:35 -0500

    assignment 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import MainBase from "./components/MainBase";
+import { EmployeePage, HomePage, LoginPage, MissingPage, RegisterPage } from "./components/pages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="auth">
+                    <Route index element={<LoginPage />} />
+                    <Route path="register" element={<RegisterPage />} />
+                </Route>
+                <Route path="/" element={<MainBase />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="employees" element={<EmployeePage />} />
+                    <Route path="*" element={<MissingPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
